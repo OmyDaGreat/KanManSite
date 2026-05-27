42722ee740eb4901d4bface61ea539710a5a63db
Om Gupta <om@malefic.xyz>
2026-05-27 13:51:51 -0700

    feat: init from static site template

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+rootProject.name = "staticsite"
+
+include(":site")
